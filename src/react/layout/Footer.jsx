2c0cef299b72899427cefdfcc9cb21843aa48754
Abdul Wahab <abdulwahabse@outlook.com>
2023-07-13 12:08:11 +0200

    feature: footer

--- src/react/layout/Footer.jsx
+++ src/react/layout/Footer.jsx
@@ -1,8 +1,42 @@
+import { Link } from 'react-router-dom';
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className="footer">
+            <div className="container footer__container">
+                <p className="typography-4 color-light footer__copyright">
+                    Copyright © 2023 What to Watch. All rights reserved
+                </p>
+                <div className="footer__links">
+                    <Link
+                        to="/"
+                        className="typography-3 color-light footer__link "
+                    >
+                        Privacy Policy
+                    </Link>
+                    <Link
+                        to="/"
+                        className="typography-3 color-light footer__link "
+                    >
+                        Careers
+                    </Link>
+                    <Link
+                        to="/"
+                        className="typography-3 color-light footer__link "
+                    >
+                        Help
+                    </Link>
+                </div>
+                <p className="typography-3 color-light">
+                    Coded by{' '}
+                    <a
+                        className="footer__credit-name color-lightest"
+                        href="https://www.linkedin.com/in/abdulwahabse/"
+                    >
+                        Abdul Wahab
+                    </a>
+                </p>
+            </div>
+        </footer>
     );
 }
 
