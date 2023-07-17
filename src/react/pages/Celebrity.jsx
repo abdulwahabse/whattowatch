b2823144c24d93fadf6dadca9764ae4a7f2dd1c9
Abdul Wahab <abdulwahabse@outlook.com>
2023-07-17 19:15:13 +0200

    feature: more details about celebrities

--- src/react/pages/Celebrity.jsx
+++ src/react/pages/Celebrity.jsx
@@ -8,6 +8,23 @@ import BackButton from './../components/common/BackButton';
 function Celebrity() {
     const { name } = useParams();
     const celebrityDetails = getCelebrityByName(name);
+    console.log('celebrityDetails', celebrityDetails);
+    const calculateAge = (dob) => {
+        const today = new Date();
+        const birthDate = new Date(dob);
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+
+        if (
+            monthDiff < 0 ||
+            (monthDiff === 0 && today.getDate() < birthDate.getDate())
+        ) {
+            age--;
+        }
+
+        return age;
+    };
 
     const titlesToRender = celebrityDetails.appearedIn.map((title, index) => (
         <Title {...title} key={index} />
@@ -15,14 +32,56 @@ function Celebrity() {
     return (
         <div className="celebrity container">
             <BackButton />
-            <SectionHeading>Celebrity</SectionHeading>
+            <SectionHeading className="celebrity__heading">
+                Celebrity
+            </SectionHeading>
             <div className="celebrity__info">
                 <Avatar src={celebrityDetails.info.picture} size="lg" />
                 <p className="typography-2 color-lightest celebrity__name">
                     {celebrityDetails.info.name}
                 </p>
             </div>
-            <h3 className="typography-2 color-light">Appeared In</h3>
+
+            <h3 className="typography-2 color-light celebrity__heading">
+                Overview
+            </h3>
+            <div className="celebrity__overview-container">
+                {celebrityDetails.info.dob && (
+                    <>
+                        <p className="typography-3 color-lightest celebrity__overview">
+                            <span>Age: </span>
+                            <span>
+                                {calculateAge(celebrityDetails.info.dob)} years
+                                old
+                            </span>
+                        </p>
+                        <hr className="celebrity__overview-separator" />
+                    </>
+                )}
+
+                {celebrityDetails.info.birthplace && (
+                    <>
+                        <p className="typography-3 color-lightest celebrity__overview">
+                            <span>Birthplace: </span>
+                            <span>{celebrityDetails.info.birthplace}</span>
+                        </p>
+                        <hr className="celebrity__overview-separator" />
+                    </>
+                )}
+
+                {celebrityDetails.info.height && (
+                    <>
+                        <p className="typography-3 color-lightest celebrity__overview">
+                            <span>Height: </span>
+                            <span>{celebrityDetails.info.height}</span>
+                        </p>
+                    </>
+                )}
+            </div>
+
+            <h3 className="typography-2 color-light celebrity__heading">
+                Appeared In
+            </h3>
             <div className="celebrity__titles">{titlesToRender}</div>
         </div>
     );
